fc17f9886f4cd106900652a9805047a85148c790
Dominic Hui <dominicjui112@gmail.com>
2017-12-24 00:40:25 +0000

    completed large hero and intro block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+ fs = require("fs"),
+cssImport = require('postcss-import'),
+    normalize = require("postcss-normalize"),
+mixins = require("postcss-mixins");
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
